07a7cfba1e216c9d9b4c490e64777f25c55ef928
naka-c1024 <ny42800824@gmail.com>
2022-03-12 10:33:02 +0900

    add: exitコマンドのatol以外の実装は終わった

--- builtin_dir/exit.c
+++ builtin_dir/exit.c
@@ -38,14 +38,71 @@ void	split_free(char **ptr)
 	}
 	free(ptr);
 }
+/* ---------------------------------------------------------------- */
+int	read_sign(char c)
+{
+	if (c == '-')
+		return (-1);
+	else
+		return (1);
+}
+
+bool	check_arg_value(char *str)
+{
+	size_t	i;
+	size_t	len;
+	int		sign;
+	long long	abs_val;
+
+	abs_val = 0;
+	i = 0;
+	len = ft_strlen(str);
+	sign = 1;
+	if (str[i] == '+' || str[i] == '-')
+		sign = read_sign(str[i++]);
+	while (ft_isdigit(str[i]))
+	{
+		if (sign == 1 && abs_val > (LONG_MAX - (str[i] - '0')) / 10)
+			return (false);
+		if (sign == -1 && (-1 * abs_val) < (LONG_MIN + (str[i] - '0')) / 10)
+			return (false);
+		abs_val *= 10;
+		abs_val += str[i] - '0';
+		i++;
+	}
+	if (len != i)
+		return (false);
+	return (true);
+}
 
 int	my_exit(char **split_ln)
 {
-	int	exit_status;
+	size_t	cnt;
+	long	arg_value;
 
-	write(STDERR_FILENO, "exit\n", 5);
-	exit_status = EXIT_SUCCESS;
-	return (exit_status);
+	cnt = 0;
+	while (split_ln[cnt] != NULL && cnt < 3)
+		cnt++;
+	if (cnt == 1)
+	{
+		ft_putendl_fd("exit", STDERR_FILENO);
+		return (EXIT_SUCCESS);
+	}
+	else if (cnt > 2)
+	{
+		ft_putendl_fd("exit", STDERR_FILENO);
+		ft_putendl_fd("bash: exit: too many arguments", STDERR_FILENO);
+		return (1);
+	}
+	else if (check_arg_value(split_ln[1]) == false) // オーバーフローと文字を見る
+	{
+		ft_putendl_fd("exit", STDERR_FILENO);
+		printf("bash: exit: %s: numeric argument required\n", split_ln[1]);
+		return (255);
+	}
+	arg_value = atol(split_ln[1]); // 実装する
+	ft_putendl_fd("exit", STDERR_FILENO);
+	return (arg_value);
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -53,10 +110,12 @@ int	main(int argc, char **argv, char **envp)
 	char	*line;
 	char	**split_ln;
 	int		exit_status;
+	pid_t pid;
+	int status;
 
 	while (1)
 	{
-		line = readline("exit test: "); // 入力受付
+		line = readline("\033[33m""exit test: ""\033[m"); // 入力受付
 		if (ft_strlen(line) == 0) // 改行だけの場合,空文字列がくる
 		{
 			safe_free(&line);
@@ -71,11 +130,14 @@ int	main(int argc, char **argv, char **envp)
 		if (ft_strncmp(split_ln[0], "exit", 5) == 0)
 		{
 			exit_status = my_exit(split_ln);
-			split_free(split_ln);
-			safe_free(&line);
-			return (exit_status);
+			if (exit_status != 1)
+			{
+				split_free(split_ln);
+				safe_free(&line);
+				return (exit_status);
+			}
 		}
-		else
+		else // exitしか受け付けない
 		{
 			printf("Bad argument\n");
 		}
