d43276e83713e67f9ea2106dff6e675b488f8dab
naka-c1024 <ny42800824@gmail.com>
2022-03-17 17:05:53 +0900

    update: env/exportの=がない挙動に合わせて,valueがない場合は出力しないように変更

--- builtin_dir/export.c
+++ builtin_dir/export.c
@@ -113,6 +113,11 @@ int	my_env(t_envlist *envlist)
 {
 	while (envlist)
 	{
+		if (envlist->value == NULL)
+		{
+			envlist = envlist->next;
+			continue ;
+		}
 		printf("%s=%s\n", envlist->key, envlist->value);
 		envlist = envlist->next;
 	}
