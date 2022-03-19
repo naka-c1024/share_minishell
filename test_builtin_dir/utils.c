e3c22f813fd5a9e5df32fc38c91f9d1b78ff08f6
naka-c1024 <ny42800824@gmail.com>
2022-03-19 10:54:49 +0900

    update: print_errorの修正

--- test_builtin_dir/utils.c
+++ test_builtin_dir/utils.c
@@ -2,10 +2,12 @@
 
 void	print_error(char *cmd, char *cmd_arg, int error_number)
 {
-	// printf("my_shell: %s: %s: %s\n", cmd, cmd_arg, strerror(error_number));
 	ft_putstr_fd("my_shell: ", STDERR_FILENO);
-	ft_putstr_fd(cmd, STDERR_FILENO);
-	ft_putstr_fd(": ", STDERR_FILENO);
+	if (cmd)
+	{
+		ft_putstr_fd(cmd, STDERR_FILENO);
+		ft_putstr_fd(": ", STDERR_FILENO);
+	}
 	if (cmd_arg)
 	{
 		ft_putstr_fd(cmd_arg, STDERR_FILENO);
